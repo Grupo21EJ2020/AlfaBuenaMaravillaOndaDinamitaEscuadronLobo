87b25e89cb071a1ece6dee24064737363a56e404
Daniel087-pixel <mpdaniel37@gmail.com>
2020-05-21 17:02:27 -0500

    DanielMP añadi la prueba para un menu aun no definitivo

---
+++ codigosPython/tema.py
@@ -0,0 +1,30 @@
+def menu():
+
+	print ("Selecciona una opción")
+	print ("\t1 - Crear Nuevo tema")
+	print ("\t2 - Modificar tema")
+	print ("\t3 - borrar tema")
+	print ("\t9 - salir")
+ 
+ 
+while True:
+	# Mostramos el menu
+	menu()
+ 
+	# solicitamos una opción al usuario
+	opcionMenu = input("inserta un numero valor >> ")
+ 
+	if opcionMenu=="1":
+		print ("")
+		input("Has pulsado la opción 1...\npulsa una tecla para continuar")
+	elif opcionMenu=="2":
+		print ("")
+		input("Has pulsado la opción 2...\npulsa una tecla para continuar")
+	elif opcionMenu=="3":
+		print ("")
+		input("Has pulsado la opción 3...\npulsa una tecla para continuar")
+	elif opcionMenu=="9":
+		break
+	else:
+		print ("")
+		input("No has pulsado ninguna opción correcta...\npulsa una tecla para continuar")
