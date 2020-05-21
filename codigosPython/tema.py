4277658b67631f7bdd807a2bb2efeb25a121bc9c
Daniel087-pixel <mpdaniel37@gmail.com>
2020-05-21 17:27:55 -0500

    Rehice todo :)

--- codigosPython/tema.py
+++ codigosPython/tema.py
@@ -1,35 +1,13 @@
 #########################
 # Daniel Mendoza Perez  #
-#   version 0.2         #
+#   version 0.55        #
 #########################
 
-def menu():
+temas=[]
 
-	print ("Selecciona una opción")
-	print ("\t1 - Crear Nuevo tema")
-	print ("\t2 - Modificar tema")
-	print ("\t3 - borrar tema")
-	print ("\t9 - salir")
- 
- 
-while True:
-	# Mostramos el menu
-	menu()
- 
-	# solicitamos una opción al usuario
-	opcionMenu = input("inserta un numero valor >> ")
- 
-	if opcionMenu=="1":
-		print ("")
-		input("Has pulsado la opción 1...\npulsa una tecla para continuar")
-	elif opcionMenu=="2":
-		print ("")
-		input("Has pulsado la opción 2...\npulsa una tecla para continuar")
-	elif opcionMenu=="3":
-		print ("")
-		input("Has pulsado la opción 3...\npulsa una tecla para continuar")
-	elif opcionMenu=="9":
-		break
-	else:
-		print ("")
-		input("No has pulsado ninguna opción correcta...\npulsa una tecla para continuar")
+n=int(input("cuantos temas son?"))
+for i in range(n):
+    tema=input("temas:")
+    temas.append(tema)
+print(temas) 
+  
