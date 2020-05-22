9a28fc60ab237b32f6c621e5ddd00afa4afdeafc
AntonioGzz90 <antoniogzz2590@gmail.com>
2020-05-21 23:08:06 -0500

    Se agregan de nuevo los archivos tema.py y video.py por cambio de carpeta

---
+++ src/tema.py
@@ -0,0 +1,13 @@
+#########################
+# Daniel Mendoza Perez  #
+#   version 0.55        #
+#########################
+
+temas=[]
+
+n=int(input("cuantos temas son?"))
+for i in range(n):
+    tema=input("temas:")
+    temas.append(tema)
+print(temas) 
+  
