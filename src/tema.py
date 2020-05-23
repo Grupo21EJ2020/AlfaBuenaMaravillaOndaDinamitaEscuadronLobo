b69de910d8cf95913b9edfe4f9681f699c23f315
Daniel087-pixel <mpdaniel37@gmail.com>
2020-05-23 16:17:05 -0500

    Puse Clases aun falta xs

--- src/tema.py
+++ src/tema.py
@@ -1,15 +1,34 @@
-#########################
-# Daniel Mendoza Perez  #
-#   version 0.55        #
-#########################
+class Tema:
+     def __init__(self,idTema,Nombre,):
+        self.__idTema = idTema
+        self.__Nombre = Nombre
 
-import json
-temas=[]
+@property
+def idTema(self):
+    return self.__idTema
 
-n=int(input("cuantos temas son?"))
-for i in range(n):
-    tema=input("temas:")
-    temas.append(tema)
-print(temas) 
+@property
+def Nombre(self):
+    return self.__Nombre
 
-with open('archivos/temas.txt', 'w') as outfile: json.dump(temas, outfile)
+@idTema.setter
+def idTema(self,valor):
+    return self.__idTema
+
+@Nombre.setter
+def Nombre(self,valor):
+    return self.__Nombre
+
+def IngresoTema(self):
+    #para hacer lista
+    import json
+    temas=[]
+
+    n=int(input("cuantos temas son?"))
+    for i in range(n):
+       tema=input("temas:")
+       temas.append(tema)
+    print(temas) 
+
+    #para guardar en el archivo
+    with open('archivos/temas.txt', 'w') as outfile: json.dump(temas, outfile)
