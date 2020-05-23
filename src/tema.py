49efa4830fcd0677e99c2935f744ef11c543e556
Daniel087-pixel <mpdaniel37@gmail.com>
2020-05-23 17:23:47 -0500

    agregue clases a temas

--- src/tema.py
+++ src/tema.py
@@ -19,16 +19,39 @@ def idTema(self,valor):
 def Nombre(self,valor):
     return self.__Nombre
 
-def IngresoTema(self):
+def IngresoCant(self):
     #para hacer lista
     import json
-    temas=[]
+    self.__idTemas=[]
 
-    n=int(input("cuantos temas son?"))
+    n=int(input("Cuales son los ID? "))
     for i in range(n):
-       tema=input("temas:")
-       temas.append(tema)
-    print(temas) 
+       ident=input("ID: ")
+       self.__idTemas.append(ident)
+    print(self.__idTemas) 
 
     #para guardar en el archivo
-    with open('archivos/temas.txt', 'w') as outfile: json.dump(temas, outfile)
+    with open('archivos/IDtemas.txt', 'w') as outfile: json.dump(self.__idTemas, outfile)
+
+def IngresoNom(self):
+        #para hacer lista
+    import json
+    self.__Nombre=[]
+
+    n=int(input("cuantos Nombres Ingresaras? "))
+    for i in range(n):
+       tema=input("Ingresa el Nombre segun como ingresaste los ID: ")
+       self.__Nombre.append(tema)
+    print(self.__Nombre) 
+
+    #para guardar en el archivo
+    with open('archivos/Temas.txt', 'w') as outfile: json.dump(self.__Nombre, outfile)
+
+
+    
+#combino id con nombre
+#with open('archivo1.txt') as f1, open('archivo2.txt') as f2, open('archivounion.txt', 'w') as of:
+    #line_1 = f1.readlines()[0].strip()
+    #line_2 = f2.readlines()[5].strip()
+    #out_line = "{}, {}".format(line_1[:-1], line_2[1:])
+    #of.write(out_line)
