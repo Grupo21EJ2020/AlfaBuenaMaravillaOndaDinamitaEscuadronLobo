a8d9f13e552a07d33363c5dc9e9426caa60d4476
denilson <islasdeni@gmail.com>
2020-05-23 15:21:10 -0500

    Se agregaron los property de la clase

--- src/video.py
+++ src/video.py
@@ -4,3 +4,20 @@ class Video:
         self.__nombre = nombre
         self.__url = url
         self.__fechaPublicacion = fechaPublicacion
+
+    @property
+    def idVideo(self):
+        return self.__idVideo
+
+    @property
+    def nombre(self):
+        return self.__nombre
+
+    @property
+    def url(self):
+        return self.__url
+
+    @property
+    def fechaPublicacion(self):
+        return self.__fechaPublicacion
+
