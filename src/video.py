1195bb5cac4e561d00c3c25d8e55e90ba628a571
denilson <islasdeni@gmail.com>
2020-05-23 15:18:26 -0500

    Se creo la clase video

---
+++ src/video.py
@@ -0,0 +1,6 @@
+class Video:
+    def __init__(self,idVideo,nombre,url,fechaPublicacion):
+        self.__idVideo = idVideo
+        self.__nombre = nombre
+        self.__url = url
+        self.__fechaPublicacion = fechaPublicacion
