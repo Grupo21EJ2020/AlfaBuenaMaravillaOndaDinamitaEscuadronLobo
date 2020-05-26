fac1375e7f26bdb667604e9af3737b27433171e0
danna-borda02 <61365959+danna-borda02@users.noreply.github.com>
2020-05-26 18:12:23 -0500

    Agregue codigo a empleado.py

---
+++ src/empleado.py
@@ -0,0 +1,54 @@
+class Empleado:
+     def __init__(self, idEmpleado, nombre, direccion ):
+        self.__idEmpleado= idEmpleado
+        self.__nombre = nombre
+        self.__direccion = direccion 
+
+    @property  
+    def idEmpleado(self):
+        return self.__idEmpleado
+
+    @property
+    def nombre(self):
+        return self.__nombre
+
+    @property
+    def direccion(self):
+        return self.__direccion
+
+    @idEmpleado.setter
+    def idEmpleado(self,valor):
+        self.__idVideo = valor
+
+    @nombre.setter
+    def nombre(self,valor):
+        self.__nombre = valor
+
+    @direccion.setter
+    def direccion(self,valor):
+        self.__direccion = valor
+
+    Menu=1
+    while Menu != 6:
+          print("MENU")
+          print("1)Agregar Empleado")
+          print("2)Eliminar Empleado")
+          print("3)Modificar Empleado")
+          print("4)Consultar Empleado")
+          print("5)Ver detalles de Empleados")
+          Menu=int(input("Seleción: "))
+
+    if Menu == 1:
+        print ("Registro de Empleado")
+        archivo = open("./archivos/empleados.txt","a",encoding="utf8")
+
+        print("Clave del Empleado ")
+        idEmpleado = input("Id \n")
+        print("Nombre del Empleado:\n")
+        nombre = input("Nombre: \n")
+        print("Direccion del Empleado")
+        print("Direccion del Empleado")
+        direccion = input("> ")
+
+        archivo.write(idempleado + "|" + nombre + "|" + direccion)
+        archivo.close()
