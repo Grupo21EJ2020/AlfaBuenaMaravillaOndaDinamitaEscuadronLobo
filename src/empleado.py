e069625aa15d1ff6b7e6babca048e98a0518bcdf
danna-borda02 <61365959+danna-borda02@users.noreply.github.com>
2020-05-25 22:54:35 -0500

    Añadi clase empleado

---
+++ src/empleado.py
@@ -0,0 +1,5 @@
+class Empleado:
+     def __init__(self, idEmpleado, nombre, direccion ):
+        self.__idEmpleado= idEmpleado
+        self.__nombre = nombre
+        self.__direccion = direccion 
