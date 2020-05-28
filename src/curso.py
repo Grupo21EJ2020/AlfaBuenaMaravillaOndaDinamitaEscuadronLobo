0e34357363373ab6e35b2448451da898d5e6af1f
AntonioGzz90 <antoniogzz2590@gmail.com>
2020-05-27 21:03:06 -0500

    Ya arregle lo de Aranza

--- src/curso.py
+++ src/curso.py
@@ -31,54 +31,6 @@ class Curso:
     global lista
     lista=list()
 
-    def Menu():
-        while True:
-            print(" *** MENU DEL CURSO *** ")
-            print("1.- Agregar Curso")
-            print("2.- Eliminar Curso")
-            print("3.- Modificar Curso")
-            print("4.- Ver todos los cursos")
-            print("5.- Ver algun curso")
-            print("6.- Salir")
-            Menu=int(input("Seleción: "))
-            primerID = 0
-            textoPrincipal = ""
-            segundoiD = 0 
-            clase = Curso(primerID,textoPrincipal,segundoiD)
-            
-            if Menu == 1:
-                primerID = int(input("Dime el id del Curso: "))
-                textoPrincipal = input("Dime el texto que quieres:")
-                segundoiD = int(input("Dime el id del Empleado: "))
-                clase = Curso(primerID, textoPrincipal, segundoiD)
-                clase.agregarCurso()
-                print("\n")
-            
-            elif Menu == 2:
-                clase.eliminarCurso()
-                print("\n")
-            elif Menu == 3:
-                numero = int(input("Dime el id del que quieres modificar: "))
-                idAModificar = int(input("Dime el id que quieres poner: "))
-                textoAModificar = input("Dime el nombre del curso: ")
-                idAModificarEmpleado = int(input("Dime el id del Empleado: "))
-                clase.modificarCurso(numero,idAModificar,textoAModificar, idAModificarEmpleado)
-                print("\n")
-                
-            elif Menu == 4:
-                clase.leerTodoCurso()
-                print("\n")
-            
-            elif Menu == 5:
-                clase.leeDetallesCurso()
-                print("\n")
-
-            elif Menu == 6:
-                break
-
-            else:
-                print("Ha escogido un numero erroneo\m")
-
     def agregarCurso (self):
         origen = open("./archivos/curso.txt", "a", encoding = "utf8")
         origen.write(f"{self.__idCurso}|{self.__descripcion}|{self.__idEmpleado} \n")
@@ -126,7 +78,53 @@ class Curso:
                 print(f"Este es el curso que escogio: {line}")
         archivo.close()
 
-    
+    def Menu():
+        while True:
+            print(" *** MENU DEL CURSO *** ")
+            print("1.- Agregar Curso")
+            print("2.- Eliminar Curso")
+            print("3.- Modificar Curso")
+            print("4.- Ver todos los cursos")
+            print("5.- Ver algun curso")
+            print("6.- Salir")
+            Menu=int(input("Seleción: "))
+            primerID = 0
+            textoPrincipal = ""
+            segundoiD = 0 
+            clase = Curso(primerID,textoPrincipal,segundoiD)
+            
+            if Menu == 1:
+                primerID = int(input("Dime el id del Curso: "))
+                textoPrincipal = input("Dime el texto que quieres:")
+                segundoiD = int(input("Dime el id del Empleado: "))
+                clase = Curso(primerID, textoPrincipal, segundoiD)
+                clase.agregarCurso()
+                print("\n")
+            
+            elif Menu == 2:
+                clase.eliminarCurso()
+                print("\n")
+            elif Menu == 3:
+                numero = int(input("Dime el id del que quieres modificar: "))
+                idAModificar = int(input("Dime el id que quieres poner: "))
+                textoAModificar = input("Dime el nombre del curso: ")
+                idAModificarEmpleado = int(input("Dime el id del Empleado: "))
+                clase.modificarCurso(numero,idAModificar,textoAModificar, idAModificarEmpleado)
+                print("\n")
+                
+            elif Menu == 4:
+                clase.leerTodoCurso()
+                print("\n")
+            
+            elif Menu == 5:
+                clase.leeDetallesCurso()
+                print("\n")
+
+            elif Menu == 6:
+                break
+
+            else:
+                print("Ha escogido un numero erroneo\m")
 
 
 
